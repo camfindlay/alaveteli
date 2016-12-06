15a8138ba6c8e3d4db234691a55966cf019fd2ba
Louise Crow <louise.crow@gmail.com>
2016-12-06 13:31:45 +0000

    fixup! Add InfoRequestsController#index

--- spec/models/info_request/state_spec.rb
+++ spec/models/info_request/state_spec.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe InfoRequest::States do
+describe InfoRequest::State do
 
   describe :all do
 
